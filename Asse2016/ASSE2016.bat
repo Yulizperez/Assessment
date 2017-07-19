3bfcd144e41dd2438b8c7e4b85b240f220bbf379
Yulizperez <1iza2perez@gmail.com>
2017-07-19 09:27:25 -0500

    Update ASSE2016.bat

--- Asse2016/ASSE2016.bat
+++ Asse2016/ASSE2016.bat
@@ -1,5 +1,5 @@
 pause
-sqlcmd -S EXPSATURNO1 -d master -i %CD%\assessment2016.sql -m 1 > %CD%\EXPSATURNO1_Sis&Config.txt
+sqlcmd -S EXPSATURNO1 -d master -i %CD%\assessment2016.sql -m 1 > %CD%\EXPSATURNO1.txt
 sqlcmd -S EXPSATURNO1 -d master -i %CD%\SQL_Usuario.sql -m 1 > %CD%\EXPSATURNO1_USUARIOS.txt
 sqlcmd -S EXPSATURNO1 -d master -i %CD%\LinkedServers.sql -m 1 > %CD%\EXPSATURNO1_LinkedServers.txt
 pause
